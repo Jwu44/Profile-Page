1ca1b3694ce77aacec9b3043d85cec234d66de7c
Jwu <justin.wu4444@gmail.com>
2022-05-17 11:31:25 +1000

    Updates

---
+++ precache-manifest.f0da3adc356100026880040371abf165.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "47d816049fdaeccff8d3",
+    "url": "/Profile-Page/static/css/main.034ebd9f.chunk.css"
+  },
+  {
+    "revision": "47d816049fdaeccff8d3",
+    "url": "/Profile-Page/static/js/main.cd4a051c.chunk.js"
+  },
+  {
+    "revision": "61d5e770d044a30930e5",
+    "url": "/Profile-Page/static/js/runtime~main.b1be4e08.js"
+  },
+  {
+    "revision": "ec4cbb2e2b4608057490",
+    "url": "/Profile-Page/static/js/2.4c3420ff.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/Profile-Page/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "301010364880ec3501fa60fff2691c00",
+    "url": "/Profile-Page/static/media/HackerRank_logo.30101036.svg"
+  },
+  {
+    "revision": "f35a9aba9ac2de859827782205324b7d",
+    "url": "/Profile-Page/static/media/Melbs Portrait.f35a9aba.png"
+  },
+  {
+    "revision": "e08fbf4eb2a6069701dae9c1e2d559f3",
+    "url": "/Profile-Page/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b034716178a32160f28d5d029205490.js"
+  "/Profile-Page/precache-manifest.f0da3adc356100026880040371abf165.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Profile-Page/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
